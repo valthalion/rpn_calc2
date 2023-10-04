17d312202b535c70eafe93325645851142b402b1
Diego Diaz <valthalion@gmail.com>
2023-10-04 15:38:31 +0200

    Add operators and  memory, improve aliases and look

--- ops.py
+++ ops.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
-from operator import add, sub, mul, truediv
+import math
+import operator
 from typing import Callable, Optional, Tuple
 
 from PySimpleGUI import WIN_CLOSED
@@ -24,11 +25,33 @@ class Operator:
 
 
 operators = {
-    '+': Operator(opcode='+', arity_in=2, arity_out=1, func=add, doc='Addition'),
-    '-': Operator(opcode='-', arity_in=2, arity_out=1, func=sub, doc='Subtraction'),
-    '*': Operator(opcode='*', arity_in=2, arity_out=1, func=mul, doc='Multiplication'),
-    '/': Operator(opcode='/', arity_in=2, arity_out=1, func=truediv, doc='Division'),
+    '+': Operator(opcode='+', arity_in=2, arity_out=1, func=operator.add, doc='Addition'),
+    '-': Operator(opcode='-', arity_in=2, arity_out=1, func=operator.sub, doc='Subtraction'),
+    '*': Operator(opcode='*', arity_in=2, arity_out=1, func=operator.mul, doc='Multiplication'),
+    '/': Operator(opcode='/', arity_in=2, arity_out=1, func=operator.truediv, doc='Division'),
     '^': Operator(opcode='^', arity_in=2, arity_out=1, func=pow, doc='Power'),
+    'ln': Operator(opcode='ln', arity_in=1, arity_out=1, func=math.log, doc='Natural logarithm'),
+    'exp': Operator(opcode='exp', arity_in=1, arity_out=1, func=math.exp, doc='Exponential function'),
+    '2^n': Operator(opcode='2^n', arity_in=1, arity_out=1, func=(lambda n: pow(2, n)), doc='nth power of 2'),
+    'log': Operator(opcode='log', arity_in=1, arity_out=1, func=(lambda x: math.log(x, 10)), doc='Base-10 logarithm'),
+    'log2': Operator(opcode='log2', arity_in=1, arity_out=1, func=(lambda x: math.log(x, 2)), doc='Base-2 logarithm'),
+    'sqrt': Operator(opcode='sqrt', arity_in=1, arity_out=1, func=math.sqrt, doc='Square root'),
+    'abs': Operator(opcode='abs', arity_in=1, arity_out=1, func=abs, doc='Absolute value'),
+    'floor': Operator(opcode='floor', arity_in=1, arity_out=1, func=math.floor, doc='Floor (round down)'),
+    'ceil': Operator(opcode='ceil', arity_in=1, arity_out=1, func=math.ceil, doc='Ceiling (round up)'),
+    'mod': Operator(opcode='mod', arity_in=2, arity_out=1, func=operator.mod, doc='Module'),
+    'sin': Operator(opcode='sin', arity_in=1, arity_out=1, func=math.sin, doc='Sine'),
+    'cos': Operator(opcode='cos', arity_in=1, arity_out=1, func=math.cos, doc='Cosine'),
+    'tan': Operator(opcode='tan', arity_in=1, arity_out=1, func=math.tan, doc='Tangent'),
+    'asin': Operator(opcode='asin', arity_in=1, arity_out=1, func=math.asin, doc='Arcsine'),
+    'acos': Operator(opcode='acos', arity_in=1, arity_out=1, func=math.acos, doc='Arccosine'),
+    'atan': Operator(opcode='atan', arity_in=1, arity_out=1, func=math.atan, doc='Arctangent'),
+    'inv': Operator(opcode='inv', arity_in=1, arity_out=1, func=(lambda x: 1/x), doc='Inverse (1/x)'),
+    'factorial': Operator(opcode='factorial', arity_in=1, arity_out=1, func=(lambda x: math.factorial(int(x))), doc='Factorial (x!)'),
+    'pi': Operator(opcode='pi', arity_in=0, arity_out=1, func=(lambda: math.pi), doc='The constant pi'),
+    'e_constant': Operator(opcode='e_constant', arity_in=0, arity_out=1, func=(lambda: math.e), doc='The constant e'),
+    'R2D': Operator(opcode='R2D', arity_in=1, arity_out=1, func=(lambda x: 180 * x / math.pi), doc='Convert radians to degrees'),
+    'D2R': Operator(opcode='D2R', arity_in=1, arity_out=1, func=(lambda x: math.pi * x / 180), doc='Convert degrees to radians'),
     '+/-': Operator(opcode='+/-', arity_in=1, arity_out=1, func=toggle_sign, doc='Change sign'),
     'Swap': Operator(opcode='Swap', arity_in=2, arity_out=2, func=swap, doc='Swap two top items in stack'),
     'Drop': Operator(opcode='Drop', arity_in=1, arity_out=0, func=None, doc='Remove the top item from stack'),
@@ -36,18 +59,49 @@ operators = {
 
 
 aliases = {
+    '!': 'factorial',
+    '%': 'mod',
+    'a': 'abs',
+    'c': 'ceil',
+    'd': 'R2D',
+    'e': 'EEX',
+    'f': 'floor',
+    'g': 'log',
+    'i': 'inv',
+    'l': 'ln',
+    'm': '+/-',
+    'r': 'D2R',
+    'p': 'pi',
+    's': 'sqrt',
+    'x': 'exp',
+}
+
+commands = {
+    'A': 'Aliases',
+    'C': 'Clear',
+    'D': 'Drop',
+    'E': 'e_constant',
+    'H': 'Help',
+    'L': 'Load',
+    'M': 'Save',
+    'S': 'Swap',
+    'X': 'ClrMem',
+}
+
+other_aliases = {
     'plus': '+', 'KP_Add': '+',
     'minus': '-', 'KP_Subtract': '-',
     'asterisk': '*', 'KP_Multiply': '*',
     'slash': '/', 'KP_Divide': '/',
     'asciicircum': '^',
-    'q': WIN_CLOSED, 'Q': WIN_CLOSED, 'Quit': WIN_CLOSED,
     'Return': 'Enter', 'KP_Enter': 'Enter', '\r': 'Enter',
-    'Delete': 'Clear', 'c': 'Clear', 'C': 'Clear',
-    'D': 'Drop', 'd': 'Drop', 'BackSpace': 'Drop',
-    's': 'Swap', 'S': 'Swap',
-    'm': '+/-', 'M': '+/-',
     'period': '.', 'KP_Decimal': '.',
-    'e': 'E',
-    'h': 'Help', 'H': 'Help',
+    'Delete': 'Clear', 'clear': 'clear',
+    'BackSpace': 'Drop', 'drop': 'Drop',
+    'swap': 'Swap',
+    'Q': WIN_CLOSED, 'Quit': WIN_CLOSED,
+    'd2r': 'D2R',
+    'r2d': 'R2D',
 }
+
+all_aliases = {**aliases, **commands, **other_aliases}
--- rc.py
+++ rc.py
@@ -11,7 +11,7 @@ from typing import Any, Iterable, List, Optional, Tuple, Union
 
 import PySimpleGUI as sg
 
-from ops import aliases, operators
+from ops import aliases, all_aliases, operators
 from stack import Stack
 from errors import NotEnoughArgumentsError
 
@@ -21,16 +21,24 @@ from errors import NotEnoughArgumentsError
 
 help_text = """
 Keyboard uses the same keys as
-the GUI buttons, and these aliases:
+the GUI buttons, if available.
 
-Quit: q
-Drop: d, Backspace
-Clear: c, Del
-Swap: s
-Dup[licate]: Enter
-+/-: m
+Commands use capital letters:
+- Stack and Edition
+    - Drop: D, Backspace
+    - Clear: Del
+    - Swap: S
+    - Dup[licate]: Enter
+- Memory
+    - Save: M
+    - 
+- Misc
+    - Help: H
+    - Quit: Q
+    - Insert 'e' constant: E
+    - Operator aliases: A
 
-All commands are case-insensitive.
+All trigonometric functions use radians.
 """
 
 def toggle_sign(num):
@@ -39,228 +47,283 @@ def toggle_sign(num):
     return f'-{num}'
 
 
-sg.theme('DarkGrey13')
-sg.set_options(element_padding=(0, 0))
+def NumButton(*args, **kwargs):
+    return sg.Button(*args, button_color=('white', 'steel blue'), **kwargs)
 
 
-################################################################################
-# Read options and set parameters
-
-options = '' if len(argv) <= 1 else argv[1]
-if 'l' in options:
-    button_size = (9, 3)
-    text_size = 16
-elif 's' in options:
-    button_size = (5, 2)
-    text_size = 10
-else:
-    button_size = (8, 2)
-    text_size = 14
-keep_on_top = 't' in options
-display_size = 4
-text_width = 22
+def OpButton(*args, **kwargs):
+    return sg.Button(*args, button_color=('white', 'grey30'), **kwargs)
 
 
-def pprint_stack(stack: Stack) -> str:
-    index_width = 1 + (display_size // 10)
-    number_width = text_width - (index_width + 2)  # reserve space for index + ': '
+def CmdButton(*args, **kwargs):
+    return sg.Button(*args, button_color=('white', 'grey10'), **kwargs)
 
-    if len(stack) >= display_size:
-        return '\n'.join(
-            f'{row:>{index_width}d}: {number:>{number_width}g}'
-            for row, number in zip(range(display_size, 0, -1), stack[-display_size:])
-            )
 
-    stack_len = len(stack)
-    return '\n'.join(
-        chain(
-            (
-                ': '.join((f'{row:>{index_width}d}', ' ' * number_width))
-                for row in range(display_size, stack_len, -1)
-            ),
-            (
+def AlertButton(*args, **kwargs):
+    return sg.Button(*args, button_color=('white', 'red'), **kwargs)
+
+
+def main():
+    sg.theme('DarkGrey13')
+    sg.set_options(element_padding=(0, 0))
+
+
+    ################################################################################
+    # Read options and set parameters
+
+    options = '' if len(argv) <= 1 else argv[1]
+    if 'l' in options:
+        button_size = (9, 3)
+        text_size = 16
+    elif 's' in options:
+        button_size = (5, 2)
+        text_size = 10
+    else:
+        button_size = (8, 2)
+        text_size = 14
+    keep_on_top = 't' in options
+    display_size = 4
+    text_width = 22
+
+
+    def pprint_stack(stack: Stack) -> str:
+        index_width = 1 + (display_size // 10)
+        number_width = text_width - (index_width + 2)  # reserve space for index + ': '
+
+        if len(stack) >= display_size:
+            return '\n'.join(
                 f'{row:>{index_width}d}: {number:>{number_width}g}'
-                for row, number in zip(range(stack_len, 0, -1), stack)
+                for row, number in zip(range(display_size, 0, -1), stack[-display_size:])
+                )
+
+        stack_len = len(stack)
+        return '\n'.join(
+            chain(
+                (
+                    ': '.join((f'{row:>{index_width}d}', ' ' * number_width))
+                    for row in range(display_size, stack_len, -1)
+                ),
+                (
+                    f'{row:>{index_width}d}: {number:>{number_width}g}'
+                    for row, number in zip(range(stack_len, 0, -1), stack)
+                )
             )
         )
+
+
+    ################################################################################
+    # Declare GUI items
+
+    stack = Stack()
+    edit_line = ''
+    memory: Optional[dict] = None
+
+    error_display = sg.Text(
+        '',
+        key='error_display',
+        size=(text_width, 1),
+        font=('Fira Code', text_size),
+        text_color='red',
+        pad=(0, 3),
+        justification='l',
+    )
+
+    stack_display = sg.Text(
+        pprint_stack(stack),
+        key='stack',
+        size=(text_width, display_size),
+        font=('Fira Code', text_size),
+        pad=(0, 3),
+    )
+
+    edit_line_display = sg.Text(
+        edit_line,
+        key='edit_line',
+        size=(text_width, 1),
+        font=('Fira Code', text_size),
+        pad=(0, 3),
+        justification='r',
     )
 
 
-################################################################################
-# Declare GUI items
+    ################################################################################
+    # Specify layout & create window
 
-stack = Stack()
-edit_line = ''
+    layout = [
+        [error_display],
+        [stack_display],
+        [edit_line_display],
+        [CmdButton('Clear'), CmdButton('Drop'), CmdButton('Swap'), CmdButton('Help')],
+        [OpButton('log'), OpButton('ln'), OpButton('exp'), OpButton('^')],
+        [OpButton('sqrt'), OpButton('floor'), OpButton('ceil'), OpButton('mod')],
+        [OpButton('asin'), OpButton('acos'), OpButton('atan'), OpButton('!')],
+        [OpButton('sin'), OpButton('cos'), OpButton('tan'), OpButton('inv')],
+        [NumButton('7'), NumButton('8'), NumButton('9'), OpButton('/')],
+        [NumButton('4'), NumButton('5'), NumButton('6'), OpButton('*')],
+        [NumButton('1'), NumButton('2'), NumButton('3'), OpButton('-')],
+        [NumButton('EEX'), NumButton('0'), NumButton('.'), OpButton('+')],
+        [OpButton('Enter'), OpButton('+/-'), OpButton('pi'), OpButton('abs')],
+        [OpButton('R2D'), OpButton('D2R'), OpButton('2^n'), OpButton('log2')],
+        [CmdButton('Save'), CmdButton('Load'), CmdButton('ClrMem'), AlertButton('Quit')]
+    ]
 
-error_display = sg.Text(
-    '',
-    key='error_display',
-    size=(text_width, 1),
-    font=('Fira Code', text_size),
-    text_color='red',
-    pad=(0, 3),
-    justification='l',
-)
-
-stack_display = sg.Text(
-    pprint_stack(stack),
-    key='stack',
-    size=(text_width, display_size),
-    font=('Fira Code', text_size),
-    pad=(0, 3),
-)
-
-edit_line_display = sg.Text(
-    edit_line,
-    key='edit_line',
-    size=(text_width, 1),
-    font=('Fira Code', text_size),
-    pad=(0, 3),
-    justification='r',
-)
-
-
-################################################################################
-# Specify layout & create window
-
-layout = [
-    [error_display],
-    [stack_display],
-    [edit_line_display],
-    [sg.Button('Clear'), sg.Button('Drop'), sg.Button('Swap'), sg.Button('Quit', button_color=('white', 'red'))],
-    [sg.Button('7'), sg.Button('8'), sg.Button('9'), sg.Button('/')],
-    [sg.Button('4'), sg.Button('5'), sg.Button('6'), sg.Button('*')],
-    [sg.Button('1'), sg.Button('2'), sg.Button('3'), sg.Button('-')],
-    [sg.Button('E'), sg.Button('0'), sg.Button('.'), sg.Button('+')],
-    [sg.Button('Enter'), sg.Button('+/-'), sg.Button('Help'), sg.Button('^')]
-]
-
-window = sg.Window(
-   'RPN Calculator', layout,
-   # no_titlebar=True,
-   default_button_element_size=button_size,
-   auto_size_buttons=False,
-   grab_anywhere=True,
-   keep_on_top=keep_on_top,
-   finalize=True,
-)
-window.bind('<Key>', 'Key')
+    window = sg.Window(
+       'RPN Calculator', layout,
+       # no_titlebar=True,
+       default_button_element_size=button_size,
+       auto_size_buttons=False,
+       grab_anywhere=True,
+       keep_on_top=keep_on_top,
+       finalize=True,
+    )
+    window.bind('<Key>', 'Key')
 
 
-################################################################################
-# Event loop
+    ################################################################################
+    # Event loop
 
-while True:
-    event, values = window.read()
+    while True:
+        event, values = window.read()
 
-    # Key pressed: retrieve key value
-    if event == 'Key':
-        event = str(window.user_bind_event.keysym)
-        # Strip 'KP_' prefix from num pad numbers -> same as normal number keys
-        if len(event) == 4 and event.startswith('KP_'):
-            event = event[-1]
+        # Key pressed: retrieve key value
+        if event == 'Key':
+            event = str(window.user_bind_event.keysym)
+            # Strip 'KP_' prefix from num pad numbers -> same as normal number keys
+            if len(event) == 4 and event.startswith('KP_'):
+                event = event[-1]
 
-    # Check if the event is an alias and set the cannonical value
-    event = aliases.get(event, event)
+        # Check if the event is an alias and set the cannonical value
+        event = all_aliases.get(event, event)
 
-    if event == sg.WIN_CLOSED:
-        break
+        if event == sg.WIN_CLOSED:
+            break
 
-    # Start event evaluation (note if, elif, ... structure if adding more)
+        # Start event evaluation (note if, elif, ... structure if adding more)
 
-    if event == 'Enter':
-        if edit_line:  # Push current edit line to stack
-            if edit_line.endswith('e') or edit_line.endswith('e-'):  # Drop trailing 'e' or 'e-', assume equvalent to e0
-                edit_line = edit_line.split('e')[0]
-            stack.push(float(edit_line))
-            edit_line = ''
-            window['edit_line'].update(edit_line)
-            window['stack'].update(pprint_stack(stack))
-        elif stack:  # Not editing -> duplicate top stack item
-            stack.push(stack.peek())
-            window['stack'].update(pprint_stack(stack))
+        if event == 'Enter':
+            if edit_line:  # Push current edit line to stack
+                if edit_line.endswith('e') or edit_line.endswith('e-'):  # Drop trailing 'e' or 'e-', assume equvalent to e0
+                    edit_line = edit_line.split('e')[0]
+                stack.push(float(edit_line))
+                edit_line = ''
+                window['edit_line'].update(edit_line)
+                window['stack'].update(pprint_stack(stack))
+            elif stack:  # Not editing -> duplicate top stack item
+                stack.push(stack.peek())
+                window['stack'].update(pprint_stack(stack))
 
-    elif event == 'Clear':
-        if edit_line:  # if editing, delete edit line
-            edit_line = ''
-            window['edit_line'].update(edit_line)
-        else:  # clear the stack
-            stack.clear()
-            window['stack'].update(pprint_stack(stack))
+        elif event == 'Clear':
+            if edit_line:  # if editing, delete edit line
+                edit_line = ''
+                window['edit_line'].update(edit_line)
+            else:  # clear the stack
+                stack.clear()
+                window['stack'].update(pprint_stack(stack))
 
-    elif event == 'Drop' and edit_line:  # if not editing, will be interpreted later as an operator
-        if edit_line:  # if editing, delete last character
-            edit_line = edit_line[:-1]
-            window['edit_line'].update(edit_line)
-        else:  # drop top stack item
-            evaluate(event)
-
-    elif event == '+/-' and edit_line:  # if not editing, will be interpreted later as an operator
-        if 'e' in edit_line:  # if editing the exponent, toggle exponent sign
-            base, exp = edit_line.split('e')
-            edit_line = 'e'.join((base, toggle_sign(exp)))
-        else:  # toggle mantissa sign
-            edit_line = toggle_sign(edit_line)
-        window['edit_line'].update(edit_line)
-
-    elif event == '.':
-        if '.' in edit_line:
-            window['error_display'].update('Only one point allowed')
-            continue
-        edit_line = f'{edit_line}.'
-        window['edit_line'].update(edit_line)
-
-    elif event == 'E':
-        if 'e' in edit_line:
-            window['error_display'].update('Only one "e" allowed')
-            continue
-        if edit_line:
-            edit_line = f'{edit_line}e'
-        else:  # empty line -> interpret as 1e
-            edit_line = '1e'
-        window['edit_line'].update(edit_line)
-
-    elif event in '0123456789':
-        edit_line = f'{edit_line}{event}'
-        window['edit_line'].update(edit_line)
-
-    elif event in operators:
-        if edit_line:  # if editing, push to stack before operating; taken from Enter event
-            if edit_line.endswith('e'):  # Drop trailing 'e', assume equvalent to e0
+        elif event == 'Drop' and edit_line:  # if not editing, will be interpreted later as an operator
+            if edit_line:  # if editing, delete last character
                 edit_line = edit_line[:-1]
-            stack.push(float(edit_line))
-            edit_line = ''
+                window['edit_line'].update(edit_line)
+            else:  # drop top stack item
+                evaluate(event)
+
+        elif event == '+/-' and edit_line:  # if not editing, will be interpreted later as an operator
+            if 'e' in edit_line:  # if editing the exponent, toggle exponent sign
+                base, exp = edit_line.split('e')
+                edit_line = 'e'.join((base, toggle_sign(exp)))
+            else:  # toggle mantissa sign
+                edit_line = toggle_sign(edit_line)
+            window['edit_line'].update(edit_line)
+
+        elif event == '.':
+            if '.' in edit_line:
+                window['error_display'].update('Only one point allowed')
+                continue
+            edit_line = f'{edit_line}.'
+            window['edit_line'].update(edit_line)
+
+        elif event == 'EEX':
+            if 'e' in edit_line:
+                window['error_display'].update('Only one "e" allowed')
+                continue
+            if edit_line:
+                edit_line = f'{edit_line}e'
+            else:  # empty line -> interpret as 1e
+                edit_line = '1e'
+            window['edit_line'].update(edit_line)
+
+        elif event in '0123456789':
+            edit_line = f'{edit_line}{event}'
+            window['edit_line'].update(edit_line)
+
+        elif event in operators:
+            if edit_line:  # if editing, push to stack before operating; taken from Enter event
+                if edit_line.endswith('e'):  # Drop trailing 'e', assume equvalent to e0
+                    edit_line = edit_line[:-1]
+                stack.push(float(edit_line))
+                edit_line = ''
+                window['edit_line'].update(edit_line)
+                window['stack'].update(pprint_stack(stack))
+            operator = operators[event]
+            try:
+                args = stack.pop_iter(operator.arity_in)
+            except NotEnoughArgumentsError:
+                window['error_display'].update('Too few arguments')
+                continue
+            if operator.func is not None:
+                try:
+                    result = operator.func(*args)
+                except Exception as e:
+                    window['error_display'].update(str(e))
+                    for arg in args:
+                        stack.push(arg)
+                    continue
+            if operator.arity_out == 1:
+                stack.push(result)
+            elif operator.arity_out > 1:
+                stack.push_iter(result)
+            else:  # arity_out == 0
+                pass
+            window['stack'].update(pprint_stack(stack))
+
+        elif event == 'Help':
+            sg.popup(help_text)
+
+        elif event == 'Aliases':
+            sg.popup('\n'.join(f'{v}: {k}' for k, v in aliases.items()) )
+
+        elif event == 'Save':
+            memory = {'stack': list(stack), 'edit_line': str(edit_line)}
+            window['error_display'].update('Memory stored')
+            continue
+
+        elif event == 'Load':
+            if memory is None:
+                window['error_display'].update('No memory stored')
+                continue
+            stack.clear()
+            stack.push_iter(memory['stack'])
+            edit_line = memory['edit_line']
             window['edit_line'].update(edit_line)
             window['stack'].update(pprint_stack(stack))
-        operator = operators[event]
-        try:
-            args = stack.pop_iter(operator.arity_in)
-        except NotEnoughArgumentsError:
-            window['error_display'].update('Too few arguments')
+            window['error_display'].update('Memory restored')
             continue
-        if operator.func is not None:
-            try:
-                result = operator.func(*args)
-            except Exception as e:
-                window['error_display'].update(str(e))
-                for arg in args:
-                    stack.push(arg)
-                continue
-        if operator.arity_out == 1:
-            stack.push(result)
-        elif operator.arity_out > 1:
-            stack.push_iter(result)
-        else:  # arity_out == 0
+
+        elif event == 'ClrMem':
+            memory = None
+            window['error_display'].update('Memory cleared')
+            continue
+
+        else:
+            # print('WTF:', event, values)
             pass
-        window['stack'].update(pprint_stack(stack))
 
-    elif event == 'Help':
-        sg.popup(help_text)
+        # Clear error display with next event.
+        # Explicitly continue the loop to show error message (prevent this from clearing it)
+        window['error_display'].update('')
 
-    else:
-        #print('WTF:', event, values)
-        pass
+    window.close()
 
-    window['error_display'].update('')
 
-window.close()
+if __name__ == '__main__':
+    main()
